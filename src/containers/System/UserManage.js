7ad00f287849c2010d69f4ae66c8823ff21c71ec
NGUYEN HONG SON <nguyen.hong.son.z@takenaka.co.jp>
2024-03-19 17:28:23 +0900

    Edit #38

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
+
 class UserManage extends Component {
 
     state = {
 
     }
 
-    componentDidMount() {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    async componentDidMount() {
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
+            this.setState({
+                arrUsers: response.users
+            })
+        }
 
     }
 
 
     render() {
+        let arrUsers = this.state.arrUsers;
         return (
-            <div className="text-center">Manage users</div>
+            <div className='"users-container'>
+                <div className='title text-center'>Manage User With ERIC</div>
+                <div className='users-table mt-3 mx-1'>
+                    <table>
+                        <tr>
+                            <th>Email</th>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Address</th>
+                            <th>Actions</th>
+                        </tr>
+                        {arrUsers && arrUsers.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className='btn-edit'><i className="fas fa-pencil-alt"></i></button>
+                                        <button className='btn-delete'><i class="fas fa-trash"></i></button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </table>
+                </div>
+            </div>
         );
     }
 
