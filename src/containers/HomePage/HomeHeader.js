a74e48b834ec5f93e992202ce44621cd3b3f838d
Son <hongsonndvn2014@gmail.com>
2024-03-26 08:50:40 +0900

    Edit_#48

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 import './HomeHeader.scss';
 import logo from '../../assets/logo.svg';
 import { FormattedMessage } from 'react-intl';
+import { LANGUAGES } from '../../utils';
 
 class HomeHeader extends Component {
+
+    changeLanguage = (language) => {
+
+    }
 
     render() {
         return (
@@ -35,8 +40,8 @@ class HomeHeader extends Component {
                         </div>
                         <div className='right-content'>
                             <div className='support'><i className="fas fa-question-circle"></i><FormattedMessage id="home-header.support" /></div>
-                            <div className='language-vi'>VN</div>
-                            <div className='language-en'>EN</div>
+                            <div className='language-vi'><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                            <div className='language-en'><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
                     </div>
                 </div >
