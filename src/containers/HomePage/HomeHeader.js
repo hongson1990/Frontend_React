79163585a15e43e176c56dd13920526c88fadffb
NGUYEN HONG SON <nguyen.hong.son.z@takenaka.co.jp>
2024-03-26 18:02:24 +0900

    Edit #48_1

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -4,14 +4,17 @@ import './HomeHeader.scss';
 import logo from '../../assets/logo.svg';
 import { FormattedMessage } from 'react-intl';
 import { LANGUAGES } from '../../utils';
+import { changeLanguageApp } from '../../store/actions';
 
 class HomeHeader extends Component {
 
     changeLanguage = (language) => {
-
+        this.props.changeLanguageAppRedux(language);
     }
 
     render() {
+        let language = this.props.language;
+        console.log(language);
         return (
             <>
                 <div className='home-header-container'>
@@ -40,8 +43,8 @@ class HomeHeader extends Component {
                         </div>
                         <div className='right-content'>
                             <div className='support'><i className="fas fa-question-circle"></i><FormattedMessage id="home-header.support" /></div>
-                            <div className='language-vi'><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
-                            <div className='language-en'><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
+                            <div className={language === LANGUAGES.VI ? 'language-vi active' : 'language-vi'}><span onClick={() => this.changeLanguage(LANGUAGES.VI)}>VN</span></div>
+                            <div className={language === LANGUAGES.EN ? 'language-en active' : 'language-en'}><span onClick={() => this.changeLanguage(LANGUAGES.EN)}>EN</span></div>
                         </div>
                     </div>
                 </div >
@@ -91,12 +94,14 @@ class HomeHeader extends Component {
 
 const mapStateToProps = state => {
     return {
-        isLoggedIn: state.user.isLoggedIn
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        changeLanguageAppRedux: (language) => dispatch(changeLanguageApp(language))
     };
 };
 
---
+++ src/containers/HomePage/Section/Specialty.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './Specialty.scss';
+import { FormattedMessage } from 'react-intl';
+
+class Specialty extends Component {
+
+    changeLanguage = (language) => {
+        this.props.changeLanguageAppRedux(language);
+    }
+
+    render() {
+        return (
+            <div className='section-specialty'>
+                <div className='specialty-content'></div>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
