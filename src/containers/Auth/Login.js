ca849819c206dbc151576a3c9ee9eed6fb7ebbf6
hongson1990 <hongsonndvn2014@gmail.com>
2024-05-23 23:54:49 +0900

    #72

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -63,6 +63,12 @@ class Login extends Component {
         })
     }
 
+    handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            this.handleLogin();
+        }
+    }
+
     render() {
         return (
             <div className='login-blackground'>
@@ -84,7 +90,9 @@ class Login extends Component {
                                     type={this.state.isShowPassword ? 'text' : 'password'}
                                     placeholder='Enter your password'
                                     value={this.state.password}
-                                    onChange={(event) => this.handleOnChangePassword(event)} />
+                                    onChange={(event) => this.handleOnChangePassword(event)}
+                                    onKeyDown={(event) => this.handleKeyDown(event)}
+                                />
                                 <span onClick={() => { this.handleShowHidePassword() }} >
                                     <i className={this.state.isShowPassword ? "far fa-eye" : "far fa-eye-slash"}></i>
                                 </span>
