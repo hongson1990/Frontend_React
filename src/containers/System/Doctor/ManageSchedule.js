7e4900da41b445a2aee5a124f96461270c26a4c6
NGUYEN HONG SON <nguyen.hong.son.z@takenaka.co.jp>
2024-05-23 18:09:50 +0900

    #72-1

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
+import { FormattedMessage } from 'react-intl';
+import './ManageSchedule.scss'
 
 class ManageSchedule extends Component {
     render() {
         return (
             <>
-                <div>Manage Schedule</div>
+                <div className='manage-schedule-container'>
+                    <div className='m-s-title'>
+                        <FormattedMessage id='manage-schedule.title' />
+                    </div>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-6 form-group'>
+                                <label>Chọn bác sĩ</label>
+                                <input className='form-control' />
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>Chọn ngày</label>
+                                <input className='form-control' />
+                            </div>
+                            <div className='col-12 pick-hour-container'>
+
+                            </div>
+                            <button className='btn'>Lưu thông tin</button>
+                        </div>
+                    </div>
+                </div>
             </>
         );
     }
