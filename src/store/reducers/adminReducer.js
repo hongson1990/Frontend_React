13ee7b4e0848b485e448d720fb38d8ac0082dd27
hongson1990 <hongsonndvn2014@gmail.com>
2024-05-27 00:00:54 +0900

    #78

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -8,7 +8,8 @@ const initialState = {
     users: [],
     topDoctors: [],
     allDoctors: [],
-    allScheduleTime: []
+    allScheduleTime: [],
+    allRequiredDoctorInfor: []
 }
 
 const adminReducer = (state = initialState, action) => {
@@ -88,6 +89,16 @@ const adminReducer = (state = initialState, action) => {
             }
         case actionTypes.FETCH_ALLCODE_SCHEDULE_TIME_FAILDED:
             state.allScheduleTime = [];
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_SUCCESS:
+            state.allRequiredDoctorInfor = action.data;
+            return {
+                ...state,
+            }
+        case actionTypes.FETCH_REQUIRED_DOCTOR_INFOR_FAILDED:
+            state.allRequiredDoctorInfor = [];
             return {
                 ...state,
             }
